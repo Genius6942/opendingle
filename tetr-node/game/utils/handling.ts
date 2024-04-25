89fa155d511d98402dec9d2f5aa0761ece9a6a03
Luke <132675822+EncryptLuke@users.noreply.github.com>
2024-04-25 19:48:11 -0400

    Delete tetr-node/game directory

--- tetr-node/game/utils/handling.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  arr: 2,
-  das: 10,
-  dcd: 0,
-  sdf: 40,
-  safelock: true,
-  cancel: false,
-};
--- tetr-node/game/utils/increases.ts
+++
@@ -1,9 +0,0 @@
-export const calculateIncrease = (
-  base: number,
-  frames: number,
-  increase: number,
-  marginTime: number,
-) => {
-  const times = Math.floor(Math.max(0, frames - marginTime) / 60);
-  return base + increase * times;
-};
